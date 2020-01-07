ca4b3ada2f8e8ab8a11d585ef4cf072c919d1141
Crystal Broach <crysland@gmail.com>
2020-01-06 19:00:27 -0600

    updated readme

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConnection from '../helpers/data/connection';
 
 import Auth from '../components/Auth/Auth';
+import DogPen from '../components/DogPen/DogPen';
 import Home from '../components/Home/Home';
 import Navbar from '../components/Navbar/Navbar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 
 firebaseConnection();
 
@@ -35,11 +52,15 @@ class App extends React.Component {
     const { authed } = this.state;
     return (
       <div className="App">
-        <Navbar authed={authed} />
-        {
-          (!authed) ? <Auth /> : <Home setDogView={this.setDogView} />
-        }
-    </div>
+        <Router>
+          <Navbar authed={authed} />
+          <Switch>
+            <PrivateRoute path="/" exact component={Home} authed={authed} />
+            <PrivateRoute path="/DogPen" exact component={DogPen} authed={authed} />
+            <PublicRoute path="/auth" exact component={Auth} authed={authed} />
+          </Switch>
+        </Router>
+      </div>
     );
   }
 }
